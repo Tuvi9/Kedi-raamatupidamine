149c2ed0b6653499337c4ad97c32b9407b1c22d0
Tuvi9 <kaspupavel@gmail.com>
2024-10-29 20:05:52 +0200

    Finished initial design of the product section.

--- src/Components/Product.jsx
+++ src/Components/Product.jsx
@@ -1,35 +1,30 @@
-const productDesc = [ {
-    name: 'test1',
-},
-{
-    name: 'test2'
-},
-{
-    name: 'test3'
-},
-{
-    name: 'test4'
-},
-{
-    name: 'test5'
-},
-{
-    name: 'test6'
-}
-]
-
+import productDesc from '../productDesc.json'
 
 export default function Product() {
     return(
         <>
-        <div className=" grid grid-cols-3 grid-rows-2 h-[600px]">
+        <div className="grid justify-center grid-cols-3 grid-rows-2 h-[600px]">
             {productDesc.map(function(data) {
                 return (
-                    <div key={data.name}>
-                    <div className='border-4 p-8 w-fit'>
-                        {data.name}
+                    <div className='flex justify-center items-center h-full' key={data.name}>
+                        <div className='border-4 p-4 border-purple-700 text-white text rounded-md w-[300px] max-w-[300px] max-h-[225px] h-[225px] break-words'
+                            style={{
+                                backgroundImage: `url("${data.background}")`,
+                                backgroundSize: 'cover',
+                                backgroundPosition: 'center',
+                                backgroundRepeat: 'no-repeat',
+                            }}
+                        >
+                            <div className='text-2xl flex flex-col justify-between h-full'>
+                                {data.name}
+                                <div className='border-2 p-2 rounded-md self-end'>
+                                    <button>
+                                        Loe lähemalt
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                </div>
                 )
             })}
         </div>
