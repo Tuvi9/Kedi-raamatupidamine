62440628900833f3040725e357ed432356f7cb1a
Tuvi9 <kaspupavel@gmail.com>
2024-11-07 22:26:44 +0200

    using emailjs for form section.

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,22 +1,55 @@
+import { useRef, useEffect } from 'react'
+import emailjs from '@emailjs/browser'
+
+
+emailjs.init(import.meta.env.VITE_PUBLIC_KEY)
+
 export default function Form() {
+
+    const form = useRef();
+    useEffect(() => {
+        console.log(import.meta.env.VITE_PUBLIC_KEY)
+    }, [])
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs
+        .sendForm(import.meta.env.VITE_SERVICE_ID, import.meta.env.VITE_TEMPLATE_ID, form.current, {
+            publicKey: import.meta.env.VITE_PUBLIC_KEY,
+        })
+        .then(
+            () => {
+            console.log('Edukalt saadetud!');
+            form.current.reset();
+            },
+            (error) => {
+            console.log('Midagi läks valesti...', error.text);
+            },
+        );
+    };
+
     return(
+        <>
         <div className="flex flex-col items-center justify-center w-full max-w-md mx-auto mt-8 px-4">
             <div className='text-purple-700 font-bold xs:text-2xl text-4xl pb-[25px]'>
                 <h1>Kirjutage meile</h1>
             </div>
-            <form className="space-y-4 pb-[100px] w-full">
+            <form ref={form} onSubmit={sendEmail} className="space-y-4 pb-[100px] w-full">
                 <label className="sr-only" htmlFor="nimi">Nimi:</label>
                 <input
                     type="text"
                     id="nimi"
                     placeholder="Nimi"
+                    name='kylastaja_nimi'
                     className="w-full p-2 text-xl xs:text-lg border-2 border-purple-700 rounded-md"
                 /><br/>
                 <label className="sr-only" htmlFor='epost'>E-post:</label>
                 <input
-                    type='text'
+                    type='email'
                     id='epost'
                     placeholder="E-post"
+                    name='kylastaja_epost'
                     className="w-full p-2 text-xl xs:text-lg border-2 border-purple-700 rounded-md"
                 /><br/>
                 <label className="sr-only" htmlFor='telefon'>Telefon:</label>
@@ -24,6 +57,7 @@ export default function Form() {
                     type='text'
                     id='telefon'
                     placeholder="Telefon"
+                    name='kylastaja_telefon'
                     className="w-full p-2 text-xl xs:text-lg border-2 border-purple-700 rounded-md"
                 /><br/>
                 <label className="sr-only" htmlFor='sonum'>Sõnum:</label>
@@ -31,11 +65,16 @@ export default function Form() {
                     className='w-full text-xl xs:text-lg p-2 border-2 border-purple-700 rounded-md xs:h-[150px] h-[200px]'
                     id='sonum'
                     placeholder="Sõnum"
+                    name='kylastaja_sonum'
                 />
-                <button className='flex flex-col mx-auto border-2 p-2 px-4 rounded-md border-purple-700 bg-purple-700 text-white font-bold text-xl xs:text-lg hover:bg-purple-800 transition-colors'>
+                <button
+                    type='submit'
+                    className='flex flex-col mx-auto border-2 p-2 px-4 rounded-md border-purple-700 bg-purple-700 text-white font-bold text-xl xs:text-lg hover:bg-purple-800 transition-colors'
+                >
                     Lorem Ipsum
                 </button>
             </form>
         </div>
+        </>
     )
 }
